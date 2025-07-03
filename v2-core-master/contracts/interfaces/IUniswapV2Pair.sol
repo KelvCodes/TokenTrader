50eaa2bef717fa84c237b890d3563dd18e66b729
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-03 10:50:21 -0700

    Update IUniswapV2Pair.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
@@ -1,6 +1,4 @@
 
-    function MINIMUM_LIQUIDITY() external pure returns (uint);
-    function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
