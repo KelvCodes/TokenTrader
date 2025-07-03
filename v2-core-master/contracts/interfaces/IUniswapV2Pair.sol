eb629698cd8def94770d618c9c8d4c39a93b21c6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-03 10:49:12 -0700

    Update IUniswapV2Pair.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
@@ -1,5 +1,4 @@
 
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
     function nonces(address owner) external view returns (uint);
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
