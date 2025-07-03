e7700664f09a49987c65dc03c2a9b183ab550da0
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-03 10:52:42 -0700

    Update IUniswapV2Pair.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
@@ -1,4 +1,4 @@
- returns    function burn(address to) external returns (uint amount0, uint amount1);
+    function burn(address to) external returns (uint amount0, uint amount1);
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
     function skim(address to) external;
     function sync() external;
