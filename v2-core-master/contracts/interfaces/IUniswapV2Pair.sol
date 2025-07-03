23bcc84838705b0bfd1105b79c76d89c4ce270a2
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-03 10:48:15 -0700

    Update IUniswapV2Pair.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Pair.sol
@@ -1,7 +1,4 @@
 
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint value) external returns (bool);
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
 
