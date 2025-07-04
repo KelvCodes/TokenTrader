25acc2636511009a5b04fa82ef65331d1bbeb959
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-04 04:06:55 -0700

    Update UniswapV2Factory.sol

--- v2-core-master/contracts/UniswapV2Factory.sol
+++ v2-core-master/contracts/UniswapV2Factory.sol
@@ -1,5 +1,4 @@
-
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+V2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
