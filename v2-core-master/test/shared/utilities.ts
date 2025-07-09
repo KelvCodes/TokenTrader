6c043f4e0e096256d8a20e185d86d2be4e02c9e6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-09 01:23:16 -0700

    Update utilities.ts

--- v2-core-master/test/shared/utilities.ts
+++ v2-core-master/test/shared/utilities.ts
@@ -1,5 +1,4 @@
-, token1])),
-    keccak256(bytecode)
+, token1ytecode)
   ]
   const sanitizedInputs = `0x${create2Inputs.map(i => i.slice(2)).join('')}`
   return getAddress(`0x${keccak256(sanitizedInputs).slice(-40)}`)
