f85344faf053d5ddf0d5758483fb52e5ba69e227
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-09 01:21:04 -0700

    Update utilities.ts

--- v2-core-master/test/shared/utilities.ts
+++ v2-core-master/test/shared/utilities.ts
@@ -1,4 +1,4 @@
-56(toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')),
+chainId,address verifyingContract)')),
         keccak256(toUtf8Bytes(name)),
         keccak256(toUtf8Bytes('1')),
         1,
