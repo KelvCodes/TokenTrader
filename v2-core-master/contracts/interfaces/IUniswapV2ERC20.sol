0e440a0d04d626b9debb8cae33b34f998a79f5a4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-16 14:43:28 -0700

    Update IUniswapV2ERC20.sol

--- v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
@@ -1,5 +1,4 @@
-
-(string memory);
+emory);
     function decimals() external pure returns (uint8);
     function totalSupply() external view returns (uint);
     function balanceOf(address owner) external view returns (uint);
