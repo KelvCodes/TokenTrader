2738704b57b9ad5fb434d8113f49bfabe42c0e84
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-16 14:48:36 -0700

    Update IUniswapV2ERC20.sol

--- v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
@@ -1,4 +1,4 @@
-, address spender) external view returns (uint);
+ spender) external view returns (uint);
 
     function approve(address spender, uint value) external returns (bool);
     function transfer(address to, uint value) external returns (bool);
