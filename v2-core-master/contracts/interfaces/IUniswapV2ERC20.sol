5770e78cac01a137e3850cfa3d14e277d6a74ebb
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-16 14:39:48 -0700

    Update IUniswapV2ERC20.sol

--- v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
@@ -1,6 +1,6 @@
 
 
-proval(address indexed owner, address indexed spender, uint value);
+ndexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
     function name() external pure returns (string memory);
