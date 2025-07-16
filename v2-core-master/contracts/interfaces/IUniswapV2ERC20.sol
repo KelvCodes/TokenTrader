f488eb357bd315a651f89d1cfffa042bb7c9955f
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-16 14:49:43 -0700

    Update IUniswapV2ERC20.sol

--- v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2ERC20.sol
@@ -1,4 +1,4 @@
- value) external returns (bool);
+rnal returns (bool);
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
 
