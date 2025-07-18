54953ab1a7a9b45f4b18a69d368527cd321b3f5e
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-18 10:14:13 -0700

    Update UniswapV2Pair.spec.ts

--- v2-core-master/test/UniswapV2Pair.spec.ts
+++ v2-core-master/test/UniswapV2Pair.spec.ts
@@ -1,4 +1,4 @@
-wait pair.totalSupply()).to.eq(expectedLiquidity)
+
     expect(await pair.balanceOf(wallet.address)).to.eq(expectedLiquidity.sub(MINIMUM_LIQUIDITY))
     expect(await token0.balanceOf(pair.address)).to.eq(token0Amount)
     expect(await token1.balanceOf(pair.address)).to.eq(token1Amount)
