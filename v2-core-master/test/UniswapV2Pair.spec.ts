e26590f8126b33062a42e120d57023e6e102fa90
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-18 10:21:24 -0700

    Update UniswapV2Pair.spec.ts

--- v2-core-master/test/UniswapV2Pair.spec.ts
+++ v2-core-master/test/UniswapV2Pair.spec.ts
@@ -1,8 +1,4 @@
-map(n => (typeof n === 'string' ? bigNumberify(n) : expandTo18Decimals(n))))
-  optimisticTestCases.forEach((optimisticTestCase, i) => {
-    it(`optimistic:${i}`, async () => {
-      const [outputAmount, token0Amount, token1Amount, inputAmount] = optimisticTestCase
-      await addLiquidity(token0Amount, token1Amount)
+dLiquidity(token0Amount, token1Amount)
       await token0.transfer(pair.address, inputAmount)
       await expect(pair.swap(outputAmount.add(1), 0, wallet.address, '0x', overrides)).to.be.revertedWith(
         'UniswapV2: K'
