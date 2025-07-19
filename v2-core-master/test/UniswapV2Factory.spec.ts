a4e509142a9a655d5e6d41f0f3888da4cb6b9841
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-19 15:10:18 -0700

    Update UniswapV2Factory.spec.ts

--- v2-core-master/test/UniswapV2Factory.spec.ts
+++ v2-core-master/test/UniswapV2Factory.spec.ts
@@ -2,13 +2,7 @@
 
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
-    const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2512920)
-  })
-
-  it('setFeeTo', async () => {
-    await expect(factory.connect(other).setFeeTo(other.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-    await factory.setFeeTo(wallet.address)
+    conry.setFeeTo(wallet.address)
     expect(await factory.feeTo()).to.eq(wallet.address)
   })
 
