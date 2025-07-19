155e282e8a21aeb006f5a47e302730cf95279200
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-19 14:57:07 -0700

    Update UniswapV2Factory.spec.ts

--- v2-core-master/test/UniswapV2Factory.spec.ts
+++ v2-core-master/test/UniswapV2Factory.spec.ts
@@ -1,5 +1,4 @@
-
-    mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
+orn horn horn horn horn horn horn horn horn',
     gasLimit: 9999999
   })
   const [wallet, other] = provider.getWallets()
