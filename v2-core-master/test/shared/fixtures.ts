ebf9c9d54c087b3d357e9b40dbef6c77485820b4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-20 10:43:13 -0700

    Update fixtures.ts

--- v2-core-master/test/shared/fixtures.ts
+++ v2-core-master/test/shared/fixtures.ts
@@ -1,6 +1,6 @@
-ctory.getPair(tokenA.address, tokenB.address)
-  const pair = new Contract(pairAddress, JSON.stringify(UniswapV2Pair.abi), provider).connect(wallet)
 
+  const pair = new Contract(pairAddress, JSON.stringify(UniswapV2Pair.abi), provider).connect(wallet)
+ctory.getPair(t
   const token0Address = (await pair.token0()).address
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
