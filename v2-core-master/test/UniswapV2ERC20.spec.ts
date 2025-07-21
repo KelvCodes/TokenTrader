3a52ab6ddfc3e1bd4def8c59db2ce894d947487e
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-21 14:57:15 -0700

    Update UniswapV2ERC20.spec.ts

--- v2-core-master/test/UniswapV2ERC20.spec.ts
+++ v2-core-master/test/UniswapV2ERC20.spec.ts
@@ -1,6 +1,4 @@
-
-      .withArgs(wallet.address, other.address, TEST_AMOUNT)
-    expect(await token.allowance(wallet.address, other.address)).to.eq(TEST_AMOUNT)
+s)).to.eq(TEST_AMOUNT)
   })
 
   it('transfer', async () => {
