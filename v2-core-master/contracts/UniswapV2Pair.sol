bf0874c247f6b11e25df7e3376101bac01a551a5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-22 16:27:29 -0700

    Update UniswapV2Pair.sol

--- v2-core-master/contracts/UniswapV2Pair.sol
+++ v2-core-master/contracts/UniswapV2Pair.sol
@@ -1,8 +1,5 @@
 
-        _blockTimestampLast = blockTimestampLast;
-    }
-
-    function _safeTransfer(address token, address to, uint value) private {
+    _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
     }
