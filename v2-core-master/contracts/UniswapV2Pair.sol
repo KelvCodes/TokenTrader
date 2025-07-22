9025644c5eeb0c9f6edec7975858ee2f09792065
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-22 16:27:08 -0700

    Update UniswapV2Pair.sol

--- v2-core-master/contracts/UniswapV2Pair.sol
+++ v2-core-master/contracts/UniswapV2Pair.sol
@@ -1,7 +1,6 @@
 ;
     }
-
-    function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+c view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
         _reserve0 = reserve0;
         _reserve1 = reserve1;
         _blockTimestampLast = blockTimestampLast;
