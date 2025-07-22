dd86ad2217411f9916b8f2ed3cf6ed9f2fc18a66
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-22 16:30:54 -0700

    Update UniswapV2Pair.sol

--- v2-core-master/contracts/UniswapV2Pair.sol
+++ v2-core-master/contracts/UniswapV2Pair.sol
@@ -1,6 +1,4 @@
-
-                    uint numerator = totalSupply.mul(rootK.sub(rootKLast));
-                    uint denominator = rootK.mul(5).add(rootKLast);
+;
                     uint liquidity = numerator / denominator;
                     if (liquidity > 0) _mint(feeTo, liquidity);
                 }
