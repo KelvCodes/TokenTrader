3679987b9442aa91d82e490142a786a9e583d733
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-31 06:06:12 -0700

    Update IUniswapV2Factory.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Factory.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Factory.sol
@@ -1,5 +1,4 @@
-
-ddress tokenA, address tokenB) external returns (address pair);
+address tokenB) external returns (address pair);
 
     function setFeeTo(address) external;
     function setFeeToSetter(address) external;
