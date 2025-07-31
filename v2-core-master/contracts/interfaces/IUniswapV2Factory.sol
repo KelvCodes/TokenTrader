893e1334b167e8cfd5b1d13a062cd1bcbd6d78fb
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-31 06:00:48 -0700

    Update IUniswapV2Factory.sol

--- v2-core-master/contracts/interfaces/IUniswapV2Factory.sol
+++ v2-core-master/contracts/interfaces/IUniswapV2Factory.sol
@@ -1,5 +1,5 @@
 
-    function feeTo() external view returns (address);
+ on feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
