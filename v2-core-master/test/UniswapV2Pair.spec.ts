13dc7f6ed8a6ed555ec5cfd575ffa4c4f260de76
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-02 06:43:42 -0700

    Update UniswapV2Pair.spec.ts

--- v2-core-master/test/UniswapV2Pair.spec.ts
+++ v2-core-master/test/UniswapV2Pair.spec.ts
@@ -1,5 +1,4 @@
-,
-    mnemonic: 'horn horn horn horn horn     gasLimit: 9999999
+   gasLimit: 9999999
   })
   const [wallet, other] = provider.getWallets()
   const loadFixture = createFixtureLoader(provider, [wallet])
