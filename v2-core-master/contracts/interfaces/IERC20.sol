d439ff870fad50d497b29af36d5bf36832fb35ad
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-03 06:12:21 -0700

    Update IERC20.sol

--- v2-core-master/contracts/interfaces/IERC20.sol
+++ v2-core-master/contracts/interfaces/IERC20.sol
@@ -1,5 +1,5 @@
 
-    function balanceOf(address owner) external view returns (uint);
+  lanceOf(address owner) external view returns (uint);
     function allowance(address owner, address spender) external view returns (uint);
 
     function approve(address spender, uint value) external returns (bool);
