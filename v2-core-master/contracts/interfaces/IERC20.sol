748d1723115f2c5141e1c0cc46f0c5b92db9d4de
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-03 06:17:14 -0700

    Update IERC20.sol

--- v2-core-master/contracts/interfaces/IERC20.sol
+++ v2-core-master/contracts/interfaces/IERC20.sol
@@ -1,6 +1,6 @@
 
 
-    f value) external returns (bool);
+
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
 }
