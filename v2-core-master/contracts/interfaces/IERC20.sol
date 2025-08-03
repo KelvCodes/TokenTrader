4f65861d715a395daef2961b2c8f6a3f9dd67e0a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-03 05:13:13 -0700

    Update IERC20.sol

--- v2-core-master/contracts/interfaces/IERC20.sol
+++ v2-core-master/contracts/interfaces/IERC20.sol
@@ -1,4 +1,4 @@
-(address indexed from, address indexed to, uint value);
+dexed from, address indexed to, uint value);
 
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
