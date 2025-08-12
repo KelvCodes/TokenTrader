2d58bfbc7f99eddb44a079bd818bb42a06f8fdbd
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-12 08:35:32 -0700

    Update fixtures.ts

--- v2-core-master/test/shared/fixtures.ts
+++ v2-core-master/test/shared/fixtures.ts
@@ -1,10 +1,4 @@
-ct
-  
-  // Instantiate the pair contract with ABI, address, and provider
-  const pair = new Contract(pairAddress, JSON.stringify(UniswapV2Pair.abi), provider).connect(wallet)
-
-  // Determine which token is token0 and which is token1 (sorted lexicographically inside Uniswap)
-  const token0Address = (await pair.token0()).address
+ss = (await pair.token0()).address
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
