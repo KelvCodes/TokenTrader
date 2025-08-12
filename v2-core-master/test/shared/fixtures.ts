c46ea5341a9a827b85978365b8a14e81324397b5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-12 08:35:37 -0700

    Update fixtures.ts

--- v2-core-master/test/shared/fixtures.ts
+++ v2-core-master/test/shared/fixtures.ts
@@ -1,7 +1,4 @@
-ss = (await pair.token0()).address
-  const token0 = tokenA.address === token0Address ? tokenA : tokenB
-  const token1 = tokenA.address === token0Address ? tokenB : tokenA
-
+ss = 
   // Return the full set of deployed contracts and token order
   return { factory, token0, token1, pair }
 }
