f921d3a3cc92153847fb6e1fbc9bd960dd6b7059
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-12 08:30:50 -0700

    Update fixtures.ts

--- v2-core-master/test/shared/fixtures.ts
+++ v2-core-master/test/shared/fixtures.ts
@@ -1,5 +1,4 @@
-
-import UniswapV2Factory from '../../build/UniswapV2Factory.json'
+../../build/UniswapV2Factory.json'
 import UniswapV2Pair from '../../build/UniswapV2Pair.json'
 
 // Interface for the factory fixture result
