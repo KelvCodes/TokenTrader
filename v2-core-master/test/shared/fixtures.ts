4513fc57c2b8d5e3b291c2c327826f53f749d9bf
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-12 08:30:31 -0700

    Update fixtures.ts

--- v2-core-master/test/shared/fixtures.ts
+++ v2-core-master/test/shared/fixtures.ts
@@ -1,4 +1,4 @@
-ed contract ABIs and bytecode
+
 import ERC20 from '../../build/ERC20.json'
 import UniswapV2Factory from '../../build/UniswapV2Factory.json'
 import UniswapV2Pair from '../../build/UniswapV2Pair.json'
