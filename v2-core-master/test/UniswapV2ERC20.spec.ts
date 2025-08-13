d677c6560cd75e63b7e2dc36b7f83b9fe12abdd4
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-13 14:05:23 -0700

    Update UniswapV2ERC20.spec.ts

--- v2-core-master/test/UniswapV2ERC20.spec.ts
+++ v2-core-master/test/UniswapV2ERC20.spec.ts
@@ -1,6 +1,5 @@
 
-
-import { MaxUint256 } from 'ethers/constants'
+nt256 } from 'ethers/constants'
 import { bigNumberify, hexlify, keccak256, defaultAbiCoder, toUtf8Bytes } from 'ethers/utils'
 import { solidity, MockProvider, deployContract } from 'ethereum-waffle'
 import { ecsign } from 'ethereumjs-util'
