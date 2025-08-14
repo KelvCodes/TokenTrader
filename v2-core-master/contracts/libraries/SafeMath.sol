719b9834805240edf14e1f7567004b6520980e78
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-14 07:00:03 -0700

    Update SafeMath.sol

--- v2-core-master/contracts/libraries/SafeMath.sol
+++ v2-core-master/contracts/libraries/SafeMath.sol
@@ -1,4 +1,4 @@
-r performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+forming overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
 
 library SafeMath {
     function add(uint x, uint y) internal pure returns (uint z) {
