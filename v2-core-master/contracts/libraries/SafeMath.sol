98354e07d36c8379c03519b2d3b8f39d978415ef
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-14 07:10:36 -0700

    Update SafeMath.sol

--- v2-core-master/contracts/libraries/SafeMath.sol
+++ v2-core-master/contracts/libraries/SafeMath.sol
@@ -1,5 +1,4 @@
-
-    function add(uint x, uint y) internal pure returns (uint z) {
+add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, 'ds-math-add-overflow');
     }
 
