6bdcaeed7f32cbb319f3975cd54d4ed9a34cbe42
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-27 16:48:28 -0700

    Update Math.sol

--- v2-core-master/contracts/libraries/Math.sol
+++ v2-core-master/contracts/libraries/Math.sol
@@ -1,6 +1,4 @@
-
-    }
-lonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
     function sqrt(uint y) internal pure returns (uint z) {
         if (y > 3) {
             z = y;
