b0b8cf7ef9fe63ec26fd69c148b98837106a0d21
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-28 01:23:16 -0700

    Update UniswapV2ERC20.sol

--- v2-core-master/contracts/UniswapV2ERC20.sol
+++ v2-core-master/contracts/UniswapV2ERC20.sol
@@ -1,4 +1,4 @@
-]contract implements an ERC20 token with permit (EIP-2612) functionality
+implements an ERC20 token with permit (EIP-2612) functionality
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
