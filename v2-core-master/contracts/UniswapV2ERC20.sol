63654e7359cf679d288db8935e38264b705c2684
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-28 01:25:14 -0700

    Update UniswapV2ERC20.sol

--- v2-core-master/contracts/UniswapV2ERC20.sol
+++ v2-core-master/contracts/UniswapV2ERC20.sol
@@ -1,5 +1,4 @@
 
-    // Hash of the permit structure used in EIP-712 encoding
     bytes32 public constant PERMIT_TYPEHASH = 
         0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
