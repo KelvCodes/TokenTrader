56bfa26793e0242d459b207598ee413ed27a13c1
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-28 01:25:53 -0700

    Update UniswapV2ERC20.sol

--- v2-core-master/contracts/UniswapV2ERC20.sol
+++ v2-core-master/contracts/UniswapV2ERC20.sol
@@ -1,5 +1,4 @@
 
-    event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
     // Contract constructor sets up the domain separator for EIP-712 permit
