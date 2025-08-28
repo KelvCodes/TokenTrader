737c6c2ab2c151efc7737b9acea7dfd037ff5ad8
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-28 01:25:28 -0700

    Update UniswapV2ERC20.sol

--- v2-core-master/contracts/UniswapV2ERC20.sol
+++ v2-core-master/contracts/UniswapV2ERC20.sol
@@ -1,5 +1,4 @@
 
-        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
     // Mapping to track nonces for permit (prevents replay attacks)
     mapping(address => uint) public nonces;
