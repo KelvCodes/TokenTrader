4049b1a92a7c8cfcf3840cdc1c605670be75bcf7
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-29 04:57:37 -0700

    Update UniswapV2Factory.spec.ts

--- v2-core-master/test/UniswapV2Factory.spec.ts
+++ v2-core-master/test/UniswapV2Factory.spec.ts
@@ -1,4 +1,4 @@
-ting necessary libraries and dependencies
+ry libraries and dependencies
 import chai, { expect } from 'chai'
 import { Contract } from 'ethers'
 import { AddressZero } from 'ethers/constants'
