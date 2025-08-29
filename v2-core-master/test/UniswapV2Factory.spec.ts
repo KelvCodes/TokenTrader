a43373ca3318fcd3d9307a5d9db8856300d4f498
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-29 05:09:20 -0700

    Update UniswapV2Factory.spec.ts

--- v2-core-master/test/UniswapV2Factory.spec.ts
+++ v2-core-master/test/UniswapV2Factory.spec.ts
@@ -1,15 +1,1 @@
 
-other).setFeeTo(other.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-    await factory.setFeeTo(wallet.address)
-    expect(await factory.feeTo()).to.eq(wallet.address)
-  })
-
-  // Test: Only the feeToSetter can change the feeToSetter
-  it('setFeeToSetter', async () => {
-    await expect(factory.connect(other).setFeeToSetter(other.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-    await factory.setFeeToSetter(other.address)
-    expect(await factory.feeToSetter()).to.eq(other.address)
-    await expect(factory.setFeeToSetter(wallet.address)).to.be.revertedWith('UniswapV2: FORBIDDEN')
-  })
-})
-
