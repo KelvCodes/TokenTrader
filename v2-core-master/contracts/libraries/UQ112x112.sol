6cb41e7231c52dff73dba22d57047f1696f95214
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-30 12:20:54 -0700

    Update UQ112x112.sol

--- v2-core-master/contracts/libraries/UQ112x112.sol
+++ v2-core-master/contracts/libraries/UQ112x112.sol
@@ -1,7 +1,4 @@
-s
-    }
-
-    // divide a UQ112x112 by a uint112, returning a UQ112x112
+e a UQ112x112 by a uint112, returning a UQ112x112
     function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
         z = x / uint224(y);
     }
