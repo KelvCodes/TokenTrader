fb359935bf361be03d5939e0c836450fb6279087
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-30 12:14:39 -0700

    Update UQ112x112.sol

--- v2-core-master/contracts/libraries/UQ112x112.sol
+++ v2-core-master/contracts/libraries/UQ112x112.sol
@@ -1,6 +1,6 @@
 
 
-// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
 
 // range: [0, 2**112 - 1]
 // resolution: 1 / 2**112
