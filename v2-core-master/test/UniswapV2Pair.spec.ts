4272a98fb004861a810986954a48ebb8fb37be4d
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-09 16:53:47 -0700

    Update UniswapV2Pair.spec.ts

--- v2-core-master/test/UniswapV2Pair.spec.ts
+++ v2-core-master/test/UniswapV2Pair.spec.ts
@@ -1,7 +1,5 @@
 
-    const expectedLiquidity = ex
-      .withArgs(AddressZero, AddressZero, MINIMUM_LIQUIDITY)
-      .to.emit(pair, 'Transfer')
+    const expectedLi
       .withArgs(AddressZero, wallet.address, expectedLiquidity.sub(MINIMUM_LIQUIDITY))
       .to.emit(pair, 'Sync')
       .withArgs(token0Amount, token1Amount)
