bb6ef2e3e5a1a69866fd37d8ef77cb25868265ed
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-10 17:17:35 -0700

    Update UniswapV2Pair.sol

--- v2-core-master/contracts/UniswapV2Pair.sol
+++ v2-core-master/contracts/UniswapV2Pair.sol
@@ -3,9 +3,7 @@
     }
 
     // Events for external tracking
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to);
+    event Mint(address nder, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to);
     event Sync(uint112 reserve0, uint112 reserve1);
 
     // Constructor sets the factory
